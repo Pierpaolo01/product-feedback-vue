0dbef68f55d1297b4e12400e6d845c2353d975f8
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-03-21 13:15:12 +0100

    Merge conflict resolved

--- src/pages/Suggestions/SuggestionsPage.vue
+++ src/pages/Suggestions/SuggestionsPage.vue
@@ -9,25 +9,14 @@
 import { Component, Vue } from 'vue-property-decorator'
 import HeaderComponent from '@/pages/Suggestions/components/HeaderComponent.vue'
 import SortByBar from '@/pages/Suggestions/components/SortByBar.vue'
-import axios from 'axios'
+
 @Component({
   components: {
     SortByBar,
     HeaderComponent
   }
 })
-export default class SuggestionsPage extends Vue {
-  public created (): void {
-    setTimeout(() => {
-      axios.get('http://localhost:5001/api/protected-content')
-        .then((response) => {
-          if (response.status === 200) {
-            console.log(response.data)
-          }
-        })
-    }, 2000)
-  }
-}
+export default class SuggestionsPage extends Vue {}
 </script>
 
 <style scoped>
